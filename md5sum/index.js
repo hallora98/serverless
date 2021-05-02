fc2e168edc0a29a6a580a1596089e8c62ffd7f95
hallora98 <81336163+hallora98@users.noreply.github.com>
2021-05-02 16:40:03 -0400

    Update index.js

--- md5sum/index.js
+++ md5sum/index.js
@@ -16,13 +16,7 @@ exports.handler = (event, handler, callback) => {
     // added -n because echo includes a newline, causing md5sum to be wrong
     var cmd = 'echo -n "' + input + '" | md5sum | sed "s/ -//"';
     
- if (!/^[a-z0-9 .-]*$/i.test(value)) {
-        console.log('Bad input for ' + input);
-        callback(null, {
-                statusCode: 400,
-                body: "Please provide only letters, numbers, periods, dashes, and spaces"
-        });
-}
+
 
     // execute the full command
     exec(cmd, (error, stdout, stderr) => {
